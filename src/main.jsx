7af5b7032cc5c2b4aaf6bd0b9a60b0efd626b05b
tedschu <ted.schuster@gmail.com>
2024-05-15 12:43:29 -0500

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./app/App";
+import "./index.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+        <Provider store={store}>
+            <App />
+        </Provider>
+
+
+
+);
